e9fa183aef6a3aee2cf69a0ef906328988df3256
orthrin <gronzelg@gmail.com>
2020-05-26 01:03:50 +0300

    Shaman macro fix

--- 3-macro/Shaman.lua
+++ 3-macro/Shaman.lua
@@ -1,94 +1,55 @@
--- -- ------------------------------------------------------------------------------------------------------------------------
--- -- -- :: Import: System, Locales, PrivateDB, ProfileDB, GlobalDB, PeopleDB, AlertColors AddonName
--- -- local A, L, V, P, G, C, M, N = unpack(select(2, ...));
--- -- local moduleName = 'Shaman';
--- -- local moduleAlert = M .. moduleName .. ": |r";
--- -- local module = A:GetModule(moduleName);
--- -- ------------------------------------------------------------------------------------------------------------------------
--- -- -- ==== Variables
--- -- -- local shield = ''
--- -- -- local onehand = ''
--- -- -- local twohand = ''
--- -- -- local offhand = ''
+------------------------------------------------------------------------------------------------------------------------
+-- :: Import: System, Locales, PrivateDB, ProfileDB, GlobalDB, PeopleDB, AlertColors AddonName
+local A, L, V, P, G, C, M, N = unpack(select(2, ...));
+local moduleName = 'Shaman';
+local moduleAlert = M .. moduleName .. ": |r";
+local module = A:GetModule(moduleName);
+------------------------------------------------------------------------------------------------------------------------
+-- ==== Variables
+local mod = 1
 
--- -- local setEnhacement = CreateFrame("CheckButton", "enchaceButton", UIParent,
--- --                               "SecureActionButtonTemplate")
--- -- setEnhacement:SetAttribute("type", "macro")
+local setEnhancement = CreateFrame("CheckButton", "enhanceButton", UIParent,
+                                   "SecureActionButtonTemplate")
+setEnhancement:SetAttribute("type", "macro")
 
+-- ==== Methods
+function Arch_shamanWeapons()
+    local a, b, _, c, d, _ = GetWeaponEnchantInfo()
+    if mod == 4 then mod = 1 end
+    if mod == 1 then
+        if a then
+            SELECTED_CHAT_FRAME:AddMessage(
+                moduleAlert .. 'clearing enhancements')
+            enhanceButton:SetAttribute("macrotext",
+                                       "/run CancelItemTempEnchantment(1)")
+            mod = mod + 1
+            return
+        end
+        mod = mod + 1
+    end
+    if mod == 2 then
+        SELECTED_CHAT_FRAME:AddMessage(moduleAlert .. 'applying mainhand')
+        enhanceButton:SetAttribute("macrotext", "/cast Windfury Weapon")
+        mod = mod + 1
+        return
+    end
+    if mod == 3 then
+        SELECTED_CHAT_FRAME:AddMessage(moduleAlert .. 'applying offhand')
+        enhanceButton:SetAttribute("macrotext", "/cast Flametongue Weapon")
+        mod = mod + 1
+        return
+    end
+end
 
--- -- -- ==== Methods
--- -- local function handleShield(msg)
--- --     local a, b, _, c, d, _ = GetWeaponEnchantInfo() local x = 900000 if a and b < x then CancelItemTempEnchantment(1) end if c and d < x then CancelItemTempEnchantment(2) end
--- --     -- CastSpell(58790)
--- --     -- if msg ~= '' and msg ~= ' ' and msg ~= nil then shield = GetItemInfo(msg) end
--- --     -- A.global.warrior.shield = shield
--- --     -- print(A.global.warrior.shield)
--- --     -- SELECTED_CHAT_FRAME:AddMessage(moduleAlert .. 'Your current shield is ' ..
--- --     --                                    msg)
--- --     if mod == 1 then
--- --     enchaceButton:SetAttribute("macrotext", --
--- --     "#showtooltip
--- --     /cast [spec:1] Windfury Weapon
--- --     /cast [spec:2] Windfury Weapon
--- --     /use 16")
-    
+-- ==== Start
+function module:Initialize()
+    if UnitClass('player') == 'Shaman' then self.initialized = true end
+end
 
--- -- end
+-- -- ==== End
+local function InitializeCallback(msg) module:Initialize(msg) end
+A:RegisterModule(module:GetName(), InitializeCallback)
 
--- -- -- ==== Start
--- -- function module:Initialize()
--- --     if UnitClass('player') == 'Shaman' then
--- --         self.initialized = true
--- --         --
--- --         -- if A.global.warrior == nil then
--- --         --     A.global.warrior = {
--- --         --         shield = '',
--- --         --         onehand = '',
--- --         --         twohand = '',
--- --         --         offhand = ''
--- --         --     }
--- --         -- end
--- --         -- --
--- --         -- shield = A.global.warrior.shield
--- --         -- onehand = A.global.warrior.onehand
--- --         -- twohand = A.global.warrior.twohand
--- --         -- offhand = A.global.warrior.offhand
--- --         -- handleShield('')
--- --         -- handleOnehand('')
--- --         -- handleTwohand('')
--- --         -- handleOffhand('')
--- --     end
--- --     -- :: Register some events
--- -- end
+-- ==== Todo
 
--- -- -- ==== Event Handlers
-
--- -- -- ==== Slash Handlersd
--- -- SLASH_test1 = "/test"
--- -- SlashCmdList["test"] = function(msg) handleShield(msg) end
--- -- -- SLASH_onehand1 = "/onehand"
--- -- -- SlashCmdList["onehand"] = function(msg) handleOnehand(msg) end
--- -- -- SLASH_twohand1 = "/twohand"
--- -- -- SlashCmdList["twohand"] = function(msg) handleTwohand(msg) end
--- -- -- SLASH_offhand1 = "/offhand"
--- -- -- SlashCmdList["offhand"] = function(msg) handleOffhand(msg) end
-
--- -- -- -- ==== GUI
--- -- -- GameTooltip:HookScript("OnTooltipSetUnit", Archrist_PlayerDB_getRaidScore)
-
--- -- -- -- ==== End
--- -- local function InitializeCallback(msg) module:Initialize(msg) end
--- -- A:RegisterModule(module:GetName(), InitializeCallback)
-
--- -- -- ==== Todo
-
--- -- -- ==== UseCase
-
--- -- /run t=0 if t==2 then CastSpell(58790) t=0 end if t==1 then CastSpell(58504) t=2 end if t==0 then CancelItemTempEnchantment(1) CancelItemTempEnchantment(2) t=1 end 
-
-
--- /run local a,b,_,c,d,_=GetWeaponEnchantInfo() local t if a and t==nil then CancelItemTempEnchantment(1) t=1 end if c and t==1 then CancelItemTempEnchantment(2) t=nil end 
--- /castsequence reset=5 Windfury Weapon, Flametongue Weapon
-
-
--- -- CastSpellByName("Windfury Weapon", 16)
+-- ==== UseCase
