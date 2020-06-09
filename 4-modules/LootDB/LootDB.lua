e6e3e069a5976c120cc128e18b7081b8dbff8587
orthrin <gronzelg@gmail.com>
2020-06-10 02:00:42 +0300

    LootDB bugfix, update

--- 4-modules/LootDB/LootDB.lua
+++ 4-modules/LootDB/LootDB.lua
@@ -20,12 +20,62 @@ local focus = Arch_focusColor
 -- GameTooltip:HookScript("OnTooltipSetUnit", Archrist_PlayerDB_getRaidScore)
 
 -- ==== Methods
+local function getItem(msg)
+
+    local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType,
+          itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice =
+        GetItemInfo(msg)
+
+    -- if itemName and (itemType == 'Weapon' or itemType == 'Armor') then
+    item = itemLink:match("item:(%d+):")
+    rarity = itemRarity
+    link = itemLink
+    count = GetItemCount(item, nil, nil)
+    name = itemName
+    type = itemType
+    -- end
+
+    -- print(itemEquipLoc .. ' ' .. itemSubType .. ' ' .. itemType)
+
+end
+
 local function handleCommand(msg)
     if msg == 'x' then
         -- print('a')
         Arch_setGUI('LootDatabasePrune', true)
     elseif msg ~= '' then
         msg = fixArgs(msg)
+        if msg[2] then
+            local args = msg
+            local player, loot
+            local legit = false
+            -- :: defensive argumanlar var mi 
+            if args[1] then player = args[1] end
+            if args[2] then loot = args[2] end
+            -- :: player gercekten var mi kontrolu
+            for ii = 1, GetNumGuildMembers() do
+                local isExists = GetGuildRosterInfo(ii)
+                if isExists == player then
+                    legit = true
+                    break
+                end
+            end
+            --
+            if legit then
+                if not A.loot[realmName][player] then
+                    A.loot[realmName][player] = {}
+                end
+                local core = {}
+                getItem(loot)
+                core[1] = loot
+                core[2] = date("%d-%m-%y")
+                core[3] = 'Manual Entry'
+                table.insert(A.loot[realmName][player], 1, core)
+                SELECTED_CHAT_FRAME:AddMessage(
+                    moduleAlert .. link .. ' added to ' .. player)
+            end
+            return
+        end
         msg = msg[1]
         if A.loot[realmName][msg] then
             GUI_insertPerson(msg)
@@ -38,63 +88,43 @@ local function handleCommand(msg)
     end
 end
 
-local function getItem(msg)
-
-    local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType,
-          itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice =
-        GetItemInfo(msg)
-
-    -- if itemName and (itemType == 'Weapon' or itemType == 'Armor') then
-        item = itemLink:match("item:(%d+):")
-        rarity = itemRarity
-        link = itemLink
-        count = GetItemCount(item, nil, nil)
-        name = itemName
-        type = itemType
-    -- end
-
-    -- print(itemEquipLoc .. ' ' .. itemSubType .. ' ' .. itemType)
-
-end
-
 local function insertLoot(player, stuff, rarity)
     -- :: Rarity check
-    if rarity then
-        if rarity >= 4 then
-            -- :: Guild check
-            if UnitInRaid('player') then
-                for ii = 1, GetNumGuildMembers() do
-                    local playerName = GetGuildRosterInfo(ii)
-                    if playerName == player then
-                        -- :: Raid check
-                        for yy = 1, GetNumRaidMembers() do
-                            local playerName = GetRaidRosterInfo(yy)
-                            if playerName == player then
-                                -- :: Insert
-                                local db = A.loot[realmName]
-                                if not db[player] then
-                                    db[player] = {}
-                                end
-                                -- for ii = 1, #db[player] do
-                                --     if db[player][ii][1] == stuff then
-                                --         if stuff ~= "Fragment of Val'anyr" then
-                                --             return
-                                --         end
-                                --     end
-                                -- end
-
-                                -- test 
-                                if type == 'Weapon' or type == 'Armor' then
-                                end
-                                -- test
-                                loot[1] = stuff
-                                loot[2] = date("%d-%m-%y")
-                                loot[3] = name
-                                table.insert(db[player], 1, loot)
-                                SELECTED_CHAT_FRAME:AddMessage(
-                                    moduleAlert .. link .. ' added to ' ..
-                                        target)
+    if rarity >= 4 then
+        -- :: Guild check
+        if UnitInRaid('player') then
+            for ii = 1, GetNumGuildMembers() do
+                local playerName = GetGuildRosterInfo(ii)
+                if playerName == player then
+                    -- :: Raid check
+                    for yy = 1, GetNumRaidMembers() do
+                        local playerName = GetRaidRosterInfo(yy)
+                        if playerName == player then
+                            -- :: Insert
+                            local db = A.loot[realmName]
+                            if not db[player] then
+                                db[player] = {}
                             end
+                            -- :: item daha once eklenmisse kabul etmiyor
+                            for ii = 1, #db[player] do
+                                if db[player][ii][1] == stuff and
+                                    db[player][ii][2] == date("%d-%m-%y") then
+                                    if stuff ~= "Fragment of Val'anyr" then
+                                        return
+                                    end
+                                end
+                            end
+                            -- test 
+                            -- if type == 'Weapon' or type == 'Armor' then
+                            -- end
+                            -- test
+                            loot[1] = stuff
+                            loot[2] = date("%d-%m-%y")
+                            loot[3] = name
+                            table.insert(db[player], 1, loot)
+                            SELECTED_CHAT_FRAME:AddMessage(
+                                moduleAlert .. link .. ' added to ' .. target)
+                            break
                         end
                     end
                 end
@@ -135,18 +165,17 @@ function module:TRADE_CLOSED()
         itemCount = GetItemCount(item, nil, nil)
     end
     --
-    if not count then count = 0 end
+    if count == nil then count = 0 end
     --
-    if (UnitName("npc") == target) then
-        if (count > itemCount) then insertLoot(target, item, rarity) end
-    end
+    if (count > itemCount) then insertLoot(target, item, rarity) end
 end
 
 function module:TRADE_SHOW()
     target = UnitName('npc')
     if UnitInRaid('player') then
-    SELECTED_CHAT_FRAME:AddMessage(moduleAlert .. 'Please give raid loot one by one to ' ..
-                                       target)
+        SELECTED_CHAT_FRAME:AddMessage(moduleAlert ..
+                                           'Please give raid loot one by one to ' ..
+                                           target)
     end
     if GetTradePlayerItemLink(1) then
         item = GetTradePlayerItemLink(1)
