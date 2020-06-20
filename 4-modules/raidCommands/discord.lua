88ff79ca089b667aa9529fb31e97e1c1967a9282
orthrin <gronzelg@gmail.com>
2020-06-20 18:16:50 +0300

    8.5 - discord, lootrules, paladinbuffs

--- 4-modules/raidCommands/discord.lua
+++ 4-modules/raidCommands/discord.lua
@@ -1,33 +1,35 @@
--- ------------------------------------------------------------------------------------------------------------------------
--- -- Import: System, Locales, PrivateDB, ProfileDB, GlobalDB, PeopleDB, AlertColors AddonName
--- local A, L, V, P, G, C, M, N = unpack(select(2, ...));
--- local moduleName = 'discord';
--- local moduleAlert = M .. moduleName .. ": |r";
--- local module = A:GetModule(moduleName);
--- ------------------------------------------------------------------------------------------------------------------------
--- -- ==== Variables
--- local isEnabled = true
--- local string1 = "Join https://discord.gg/XCGTYtp "
--- local string2 = ">> Change your discord nickname same as wow "
--- local string3 = ">> Join [The Prancing Pony] Channel "
+------------------------------------------------------------------------------------------------------------------------
+-- Import: System, Locales, PrivateDB, ProfileDB, GlobalDB, PeopleDB, AlertColors AddonName
+local A, L, V, P, G, C, R, M, N = unpack(select(2, ...));
+local moduleName = 'Discord';
+local moduleAlert = M .. moduleName .. ": |r";
+local module = A:GetModule(moduleName);
+------------------------------------------------------------------------------------------------------------------------
+-- ==== Variables
+local isEnabled = true
+local string1 = "If you like to be informed about further runs please join https://discord.gg/wQTEexv "
+local string2 = ">> Change your discord nickname same as wow "
+local string3 = ">> Join [The Prancing Pony] Channel "
+string2, string3 = '',''
 
--- -- ==== Body
--- local function announceDiscord()
---     if isEnabled then
---         SendChatMessage(string1 .. string2 .. string3,"RAID_WARNING") -- RAID_WARNING, SAY
---     end
--- end
+-- ==== Body
+local function announceDiscord()
+    if isEnabled then
+        -- SELECTED_CHAT_FRAME:AddMessage(string1)
+        SendChatMessage(string1 .. string2 .. string3,"RAID_WARNING") -- RAID_WARNING, SAY
+    end
+end
 
--- -- ==== Slash commands [last arg]
--- SLASH_DISCORD1 = "/discord"
--- SlashCmdList["DISCORD"] = function(msg) announceDiscord() end
+-- ==== Slash commands [last arg]
+SLASH_DISCORD1 = "/discord"
+SlashCmdList["DISCORD"] = function(msg) announceDiscord() end
 
--- -- local b = CreateFrame("Button", "MyButton", UIParent, "UIPanelButtonTemplate")
--- -- b:SetSize(80 ,22)
--- -- b:SetText("Button!")
--- -- b:SetPoint("CENTER")
--- -- b:SetScript("OnClick", function()
--- --     SendChatMessage("I'm saying stuff" ,"SAY")
--- -- end)
+-- local b = CreateFrame("Button", "MyButton", UIParent, "UIPanelButtonTemplate")
+-- b:SetSize(80 ,22)
+-- b:SetText("Button!")
+-- b:SetPoint("CENTER")
+-- b:SetScript("OnClick", function()
+--     SendChatMessage("I'm saying stuff" ,"SAY")
+-- end)
 
--- -- https://www.wowinterface.com/forums/showthread.php?t=37386
+-- https://www.wowinterface.com/forums/showthread.php?t=37386
--- 4-modules/raidCommands/lootRules.lua
+++ 4-modules/raidCommands/lootRules.lua
@@ -1,23 +1,24 @@
--- ------------------------------------------------------------------------------------------------------------------------
--- -- Import: System, Locales, PrivateDB, ProfileDB, GlobalDB, PeopleDB, AlertColors AddonName
--- local A, L, V, P, G, C, M, N = unpack(select(2, ...));
--- local moduleName = 'lootRules';
--- local moduleAlert = M .. moduleName .. ": |r";
--- local module = A:GetModule(moduleName);
--- ------------------------------------------------------------------------------------------------------------------------
--- -- ==== Variables
--- local isEnabled = true
--- local string1 = "Loot Rules: Armor Prio & MS > OS & 10 point decrease next roll for every acquired item"
+------------------------------------------------------------------------------------------------------------------------
+-- Import: System, Locales, PrivateDB, ProfileDB, GlobalDB, PeopleDB, AlertColors AddonName
+local A, L, V, P, G, C, R, M, N = unpack(select(2, ...));
+local moduleName = 'LootRules';
+local moduleAlert = M .. moduleName .. ": |r";
+local module = A:GetModule(moduleName);
+------------------------------------------------------------------------------------------------------------------------
+-- ==== Variables
+local isEnabled = true
+local string1 = "Loot Rules: Armor Prio & MS > OS & PvP Roll Spec Prio if not indicated otherwise" -- & 10 point decrease next roll for every acquired item
 
--- -- ==== Body
--- local function announceTank()
---     if isEnabled then
---         SendChatMessage(string1, "RAID_WARNING") -- RAID_WARNING, SAY
---         -- "channel", nil, "5")
---         -- "RAID_WARNING")
---     end
--- end
+-- ==== Body
+local function announceTank()
+    if isEnabled then
+        SELECTED_CHAT_FRAME:AddMessage(string1)
+        SendChatMessage(string1, "RAID_WARNING") -- RAID_WARNING, SAY
+        -- "channel", nil, "5")
+        -- "RAID_WARNING")
+    end
+end
 
--- -- ==== Slash commands [last arg]
--- SLASH_LOOTRULES1 = "/loot"
--- SlashCmdList["LOOTRULES"] = function(msg) announceTank() end
+-- ==== Slash commands [last arg]
+SLASH_LOOTRULES1 = "/rules"
+SlashCmdList["LOOTRULES"] = function(msg) announceTank() end
